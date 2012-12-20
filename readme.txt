80b3a47192112ecc07a3404cbf149b09226e2d7e
Matthew Petroff <matthew@mpetroff.net>
2012-12-19 22:37:41 -0500

    Initial commit.

---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Slenderbox ===
+Contributors: petroffm
+Tags: slenderbox, lightbox, image, photo, picture
+Requires at least: 2.6
+Tested up to: 3.5
+Stable tag: 1.0.0
+License: MIT
+License URI: http://www.opensource.org/licenses/MIT
+
+Overlays images on the current page using Slenderbox, a lightweight and framework-free lightbox plugin that can be used with valid HTML5.
+
+== Description ==
+
+This plugin overlays images on the current page using [Slenderbox](http://mpetroff.github.com/slenderbox/).
+
+= 1.0 (2012-12-19) =
+* Initial release
+* Based on Slenderbox v1.0.
+
+[Plugin website](https://github.com/mpetroff/slenderbox-wp)
+
+== Installation ==
+
+For a new installation of the plugin:
+
+1. Download the zipped plugin file to your local machine
+2. Unzip the file
+3. Upload the "slenderbox" folder to your "/wp-content/plugins/" directory
+4. Activate the plugin through the "Plugins" menu in WordPress
+5. Optionally, go to the Options page and deactivate automatic lightboxing if desired
+
+If enabled, images will automatically use lightboxes. For more information about Slenderbox, see the [script's site](http://mpetroff.github.com/slenderbox/).
+
+
+If you have already installed this plugin:
+
+1. Deactivate the plugin
+2. Download the latest files
+3. Follow the new installation steps
+
+== Frequently Asked Questions ==
+
+= Why does this plugin not work for me? =
+* The problem is with your Wordpress theme mangling image code. Use another theme that doesn't interfere with image embedding.
+* You have other plugins that conflict with Slenderbox; disable your other plugins and see if that helps. If it does, re-enable each plugin one at a time, to see which one is conflicting.
+* There are possibly other reasons.
+
+== Changelog ==
+
+= 1.0 (2012-12-19) =
+* Initial release
+* Based on Slenderbox v1.0.
---
+++ slenderbox.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * Plugin Name: Slenderbox
+ * Plugin URI: https://github.com/mpetroff/slenderbox-wp
+ * Description: Overlays images on the current page using Slenderbox, a lightweight and framework-free lightbox plugin that can be used with valid HTML5.
+ * Version: 1.0.0
+ * Author: Matthew Petroff
+ * Author URI: http://www.mpetroff.net/
+ */
+
+/*
+ * Slenderbox - A Lightweight Lightbox Script
+ * Copyright (c) 2012 Matthew Petroff
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// Automatically add lightboxing by default
+add_option('slenderbox_automate', 1);
+
+// Add an admin options page under "Options"
+function slenderbox_add_options() {
+    add_options_page('Slenderbox Options', 'Slenderbox', 'administrator', __FILE__, 'slenderbox_options_page');
+}
+add_action('admin_menu', 'slenderbox_add_options');
+
+// Admin options page
+function slenderbox_options_page() {
+    // Check for admin options submission and update options
+    if ($_REQUEST['set_options']) {
+        update_option('slenderbox_automate', $_REQUEST['automate']);
+    }
+    ?>
+    
+    <div class="wrap">
+        <h2><?php _e('Slenderbox Options', 'slenderbox') ?></h2>
+        
+        <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>&amp;updated=true">
+        <p><?php _e('Slenderbox needs "data-sbox" or "data-sbox=\'REFERENCE\'" added to image links to function; this can be done either manually or automatically.', 'slenderbox') ?></p>
+        <p><input type="checkbox" value="1" name="automate" <?php echo (get_option('slenderbox_automate') == '1') ? 'checked="checked"' : '' ?> /> <?php _e('Automatically use lightboxes for image links.', 'slenderbox') ?></p>
+        <p><input type="submit" class="button-primary" name="set_options" value="<?php _e('Save Changes', 'slenderbox') ?>"/></p>
+        </form>
+    </div>
+    
+    <?php
+}
+
+// Add slenderbox stylesheet
+function slenderbox_add_style() {
+    wp_register_style('slenderbox_style', plugins_url('slenderbox.css', __FILE__));
+    wp_enqueue_style('slenderbox_style');
+}
+add_action('wp_enqueue_scripts', 'slenderbox_add_style');
+
+// Automatically insert "data-sbox='name_of_post'" into every image link, leaving existing data attributes intact
+function slenderbox_add_data_attribute($content) {
+    global $post;
+    $patterns = array();
+    $replacements = array();
+    
+    // Copy image titles to link titles
+    $patterns[0] = '/(<a(?![^>]*?data-sbox.*)(?![^>]*?title=[\'"].*)[^>]*?href=[\'"][^\'"]+?\.(?:bmp|gif|jpg|jpeg|png|webp)[\'"][^\>]*)(>.*?title=)(["].*?["]|[\'].*?[\'])(.*?<\/a>)/i';
+    $replacements[0] = '$1 title=$3$2$3$4';
+    
+    // Add data attribute
+    $patterns[1] = '/(<a(?![^>]*?data-sbox.*)[^>]*?href=[\'"][^\'"]+?\.(?:bmp|gif|jpg|jpeg|png|webp)[\'"][^\>]*)>/i';
+    $replacements[1] = '$1 data-sbox="'.$post->ID.'">';
+    
+    // Do replacements
+    $content = preg_replace($patterns, $replacements, $content);
+    return $content;
+}
+if(get_option('slenderbox_automate') == 1) {
+    add_filter('the_content', 'slenderbox_add_data_attribute', 99);
+    add_filter('the_excerpt', 'slenderbox_add_data_attribute', 99);
+}
+
+// Include script if not viewing an admin page
+if(!is_admin()) {
+    wp_enqueue_script('slenderbox', plugins_url('slenderbox.js', __FILE__));
+}
+
+?>
